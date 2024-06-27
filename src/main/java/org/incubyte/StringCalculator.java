86b77f79f0866db994953367181a55291d5d3f0a
sreeram3449 <sreeram3449@gmail.com>
2024-06-27 18:54:24 +0530

    Passed Third Test

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -2,13 +2,18 @@ package org.incubyte;
 
 public class StringCalculator {
 
-    //TODO should return sum of numbers if passed a String containing two numbers
     public static int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
-        }
-        else{
+        } else if (numbers.contains(",")) {
+            String[] tokens = numbers.split(",");
+            return toInt(tokens[0])+toInt(tokens[1]);
+        } else{
             return Integer.parseInt(numbers);
         }
+    }
+
+    private static int toInt(String token){
+        return Integer.parseInt(token);
     }
 }
--- src/test/java/org/incubyte/StringCalculatorTest.java
+++ src/test/java/org/incubyte/StringCalculatorTest.java
@@ -22,10 +22,12 @@ public class StringCalculatorTest {
     }
 
     /**
-     * Returns sum of numbers if passed a String containing two numbers
+     * Returns sum of numbers if passed a String containing two numbers seperated by coma
      */
     @Test
     public void shouldReturnSumOfNumbersOnTwoNumberString(){
         assertEquals(3,StringCalculator.add("1,2"));
     }
+
+    //TODO test should return sum of all N numbers if passed a string containing N numbers
 }
