3902efe6b32caabb9e15663ff18833281568b79c
sreeram3449 <sreeram3449@gmail.com>
2024-06-27 17:44:47 +0530

    File Structure Created

---
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -0,0 +1,4 @@
+package org.incubyte;
+
+public class StringCalculator {
+}
---
+++ src/test/java/org/incubyte/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package org.incubyte;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class StringCalculatorTest {
+    @Test
+    public void test(){
+        fail("Not yet Implemented");
+    }
+}
