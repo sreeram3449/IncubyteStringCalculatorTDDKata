84de26aa783f25c9b0a4251062fdb9252a39263f
sreeram3449 <sreeram3449@gmail.com>
2024-06-27 18:28:33 +0530

    Second Test Added

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -1,6 +1,7 @@
 package org.incubyte;
 
 public class StringCalculator {
+    //TODO return number if passed a String containing Single number
     public static int add(String numbers){
         return 0;
     }
--- src/test/java/org/incubyte/StringCalculatorTest.java
+++ src/test/java/org/incubyte/StringCalculatorTest.java
@@ -5,10 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
+    /**
+     * Returns 0 if passed an Empty String
+     */
     @Test
     public void shouldReturnZeroOnEmptyString(){
         assertEquals(0,StringCalculator.add(""));
     }
 
-    //TODO test for Single number in string should return same string
+    /**
+     * Returns number if passed a String containing Single number
+     */
+    @Test
+    public void shouldReturnNumberOnSingleNumberString(){
+        assertEquals(1,StringCalculator.add("1"));
+    }
 }
