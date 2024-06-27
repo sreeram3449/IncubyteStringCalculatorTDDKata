f556dd718def359aecd706b540e7655618525c35
sreeram3449 <sreeram3449@gmail.com>
2024-06-27 18:06:01 +0530

    First Test

--- src/main/java/org/incubyte/StringCalculator.java
+++ src/main/java/org/incubyte/StringCalculator.java
@@ -1,4 +1,8 @@
 package org.incubyte;
 
 public class StringCalculator {
+    //TODO Implement logic to return 0 on EmptyString
+    public static int add(String numbers){
+        return -1;
+    }
 }
